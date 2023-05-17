f9e713fcd1bc0822b4b01f65c0cf15575814b0c2
fluxink <pavelvolk300@gmail.com>
2023-05-17 14:12:30 +0300

    prepare for heroku

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database connection
-define('DB_HOST', 'db');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'database');
+define('DB_HOST', isset($_ENV['DB_HOST']) ? $_ENV['DB_HOST'] : 'db');
+define('DB_USER', isset($_ENV['DB_USER']) ? $_ENV['DB_USER'] : 'root');
+define('DB_PASS', isset($_ENV['DB_PASS']) ? $_ENV['DB_PASS'] : 'root');
+define('DB_NAME', isset($_ENV['DB_NAME']) ? $_ENV['DB_NAME'] : 'database');
